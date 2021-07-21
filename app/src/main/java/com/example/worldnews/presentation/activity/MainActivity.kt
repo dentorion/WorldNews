9bdcaa143da3ee23547cb5ecb38bb7c5f86eccc7
Denys <dentor2005@gmail.com>
2021-07-21 13:55:42 +0200

    Notification Work Manager

--- app/src/main/java/com/example/worldnews/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/worldnews/presentation/activity/MainActivity.kt
@@ -7,37 +7,46 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.worldnews.R
 import com.example.worldnews.databinding.ActivityMainBinding
-import com.example.worldnews.presentation.navigation.NavManager
 import com.example.worldnews.presentation.extension.navigateSafe
+import com.example.worldnews.presentation.navigation.NavManager
+import com.example.worldnews.presentation.util.NotificationWorkManager
+import com.example.worldnews.presentation.util.WORK_TAG
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @AndroidEntryPoint
-open class MainActivity: AppCompatActivity() {
+open class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
-    private lateinit var binding: ActivityMainBinding
-    @Inject lateinit var navManager: NavManager
+    private val binding: ActivityMainBinding by viewBinding()
+
+    @Inject
+    lateinit var navManager: NavManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         setTheme(R.style.AppTheme)
 
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
         initNavController()
         initActionBar()
         initBottomNavigation()
         initNavManager()
+        initNotificationWorkManager()
     }
 
     private fun initNavController() {
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.navController
     }
 
@@ -59,6 +68,23 @@ open class MainActivity: AppCompatActivity() {
             val currentFragment = navHostFragment.childFragmentManager.fragments[0]
             currentFragment?.navigateSafe(it)
         }
+    }
+
+    //
+
+    private fun initNotificationWorkManager() {
+        /**
+         * Notification message every day
+         */
+        val notificationWork = PeriodicWorkRequestBuilder<NotificationWorkManager>(
+            20, TimeUnit.HOURS
+        ).build()
+
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+            WORK_TAG,
+            ExistingPeriodicWorkPolicy.KEEP,
+            notificationWork
+        )
     }
 
     fun getNavController(): NavController = navController
--- app/src/main/java/com/example/worldnews/presentation/util/Constants.kt
+++ app/src/main/java/com/example/worldnews/presentation/util/Constants.kt
@@ -10,3 +10,5 @@ const val IMAGE_CORNER_RADIUS_1: Int = 1
 const val TIME_2HOURS_DOWNLOAD_PAUSE: Int = 7200000
 @Keep
 const val NEWS_SHARED_PREFERENCES_TAG = "DOWNLOAD_TIME"
+@Keep
+const val WORK_TAG = "WorldNewsTag"
